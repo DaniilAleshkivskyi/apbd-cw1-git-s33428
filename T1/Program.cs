3c05c3398e26f6699f6a89b941b556cc74abc2f4
DaniilAleshkivskyi <milukovdaniil11@gmail.com>
2026-03-11 16:23:24 +0100

    add implementation of main method in StatisticsHepler class

--- T1/Program.cs
+++ T1/Program.cs
@@ -1,4 +1,14 @@
-﻿class StatisticsHelper
+﻿namespace T1;
+
+class StatisticsHelper
 {
+
+
+    public static void Main(string[] args)
+    { 
+        Console.Write("Enter smth:"); 
+        var a = Console.ReadLine();
+        Console.Write(a);
+    }
     
 }
