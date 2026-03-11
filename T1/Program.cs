6564c465de0a5a762c686c8d6f1e3006feb5434c
DaniilAleshkivskyi <milukovdaniil11@gmail.com>
2026-03-11 16:24:26 +0100

    deleted implementation of main method in StatisticsHepler class :3

--- T1/Program.cs
+++ T1/Program.cs
@@ -6,9 +6,7 @@ class StatisticsHelper
 
     public static void Main(string[] args)
     { 
-        Console.Write("Enter smth:"); 
-        var a = Console.ReadLine();
-        Console.Write(a);
+        
     }
     
 }
